feb7d209b6d2ee70824c221563c3792faed17798
1K2S <51541614+1Kill2Steal@users.noreply.github.com>
2024-04-09 14:13:51 +0300

    Fixed a small logic error on negative page overflowing
    * Edited from my phone

--- src/sites/gallery.rs
+++ src/sites/gallery.rs
@@ -31,11 +31,7 @@ pub fn gallery() -> Html {
         // Edge cases handling
         let (negative, positive): (bool, bool) = (distance < 0, distance > 0);
         if negative && *counter + distance < 1 {
-            let res = if distance.abs() == 1 || distance.abs() >= total_pages {
-                total_pages
-            } else {
-                total_pages - distance.abs()
-            };
+            let res = total_pages - distance.abs() + 1;
             return Callback::from(move |_| counter.set(res));
         }
         let distance_sum = *counter + distance;
