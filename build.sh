2e7a1e6965a3c6189563f30b5f7ff1adae33aee3
1K2S <51541614+1Kill2Steal@users.noreply.github.com>
2024-04-09 09:07:20 +0300

    Set build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Adding dirs from index.html data trunks"
+mkdir hutao
+mkdir hutao/pics
+mkdir hutao/pics_uncompressed
+
+rustup default nightly
+
+echo "Adding wasm target"
+rustup target add wasm32-unknown-unknown
+
+echo "Installing trunk"
+cargo install trunk
+
+echo "Building site"
+trunk build --release
