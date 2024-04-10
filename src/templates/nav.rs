7fb80863efce6f7147b4d58645287aa5409edab1
1Kill2Steal <ivankirilov640@gmail.com>
2024-04-10 23:43:41 +0300

    Updated the navbar to be more responsive This is more beneficial later on when there're more places to navigate through.

--- src/templates/nav.rs
+++ src/templates/nav.rs
@@ -4,6 +4,25 @@ use yew_router::prelude::*;
 
 #[function_component(Nav)]
 pub fn nav() -> Html {
+    let mobile_nav = use_state(|| true);
+    let flip = || {
+        let item = mobile_nav.clone();
+        Callback::from(move |_| item.set(!*item))
+    };
+    // You may add a name to it if you want some special styling. These differences in the classes
+    // were meant to be used to handle the following edge scenario:
+    // - User opened inspect mode -> sized down the window (to reveal the mobile nav)
+    // - Then upscaled the window and forgot to reset the above flip state.
+    // It's really whatever but decided to be perfectionistic so yea.
+    let opened_nav_item = "";
+    let set_nav_item_class = || {
+        if *mobile_nav {
+            "nav-item"
+        } else {
+            opened_nav_item
+        }
+    };
+
     let nav_homepage = use_navigator().unwrap();
     let nav_details = use_navigator().unwrap();
     let nav_gallery = use_navigator().unwrap();
@@ -11,11 +30,21 @@ pub fn nav() -> Html {
     let callback_homepage = Callback::from(move |_| nav_homepage.push(&Route::Homepage));
     let callback_details = Callback::from(move |_| nav_details.push(&Route::Details));
     let callback_gallery = Callback::from(move |_| nav_gallery.push(&Route::Gallery));
+
     html! {
         <nav>
-            <button onclick={callback_homepage}>{ "Homepage" }</button>
-            <button onclick={callback_details}>{ "Details" }</button>
-            <button onclick={callback_gallery}>{ "Gallery" }</button>
+            <div key={"mobile_nav"} class={"mobile-nav"} onclick={flip()}>
+                <a href={"javascript:void(0);"}>{ "☰" }</a>
+            </div>
+            <div key={"homepage"} class={classes!{set_nav_item_class()}} onclick={callback_homepage}>
+                <a href={"javascript:void(0);"}>{ "Homepage" }</a>
+            </div>
+            <div key={"details"} class={classes!{set_nav_item_class()}} onclick={callback_details}>
+                <a href={"javascript:void(0);"}>{ "Details" }</a>
+            </div>
+            <div key={"gallery"} class={classes!{set_nav_item_class()}} onclick={callback_gallery}>
+                <a href={"javascript:void(0);"}>{ "Gallery" }</a>
+            </div>
         </nav>
     }
 }
