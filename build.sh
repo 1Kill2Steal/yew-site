f6fa08193f5e91d6550a5ca4703cee308aa683be
1Kill2Steal <ivankirilov640@gmail.com>
2024-04-14 16:14:58 +0300

    Added checks for cargo-make too

--- build.sh
+++ build.sh
@@ -3,30 +3,29 @@
 set -e # Exit the script if any builds fail.
 
 echo "Setting up the rustup toolchain"
-rustup toolchain install nightly
-rustup default nightly
+rustup toolchain install nightly &&
+	rustup default nightly
 
 echo "Adding wasm target"
-rustup target add wasm32-unknown-unknown
+rustup target add wasm32-unknown-unknown &
 
-echo "Installing cargo make"
-cargo install cargo-make
-
-echo "Running first test"
-cargo make test_1
-echo "Setting up the JSON files"
-cargo make 1
-
-echo "Checking if trunk is installed"
+echo "Installing cargo make && trunk"
 if ! which trunk >/dev/null 2>&1; then
 	echo "Installing trunk"
 	cargo install trunk
 else
 	echo "Trunk is already installed"
 fi
+if ! which cargo make >/dev/null 2>&1; then
+	echo "Installing cargo make"
+	cargo install cargo-make
+else
+	echo "Cargo make is already installed"
+fi
 
-# echo "Installing wasm-pack"
-# cargo install wasm-pack
+echo "Running first tests && utils"
+cargo make test_1 &&
+	cargo make 1
 
 echo "Building site"
 trunk build --release
